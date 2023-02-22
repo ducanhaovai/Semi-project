6f3d469b8b4875d81335a860391ae118bb2ca5ed
phucnopro1 <pt2777187@gmail.com>
2023-02-22 08:09:57 +0700

    hi
    hi

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,26 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/','\App\Http\Controllers\HomeController@index');
+Route::get('/login', function () {
+    return view('login');
 });
+
+
+Route::group(['prefix' => 'admin'], function () {
+        // Route::get("/", "TwoFaceAuthsController@index")->name("2fa_setting");
+        Route::get('/index', '\App\Http\Controllers\Admin\AdminController@index');
+        
+        Route::get('/room', '\App\Http\Controllers\Admin\RoomController@index')->name('admin.room');
+        Route::get('/room/form', '\App\Http\Controllers\Admin\RoomController@create')->name('admin.form');
+        Route::get('/room/{id}',  '\App\Http\Controllers\Admin\RoomController@show')->name('admin.detail');
+        // Route::get('/{path?}', function($path = null){
+        //     return View::make('admin.index');
+        // })->where('path', '.*');
+    }
+);
