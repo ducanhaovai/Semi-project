519919ccd361171cf5c2d931c0fff6f34847a357
toankhanhhttq <111647165+toankhanhhttq@users.noreply.github.com>
2023-03-17 01:59:49 +0700

    ktoan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,7 +21,7 @@
                     <option value="">San Francisco USA</option>
                     <option value="">Berlin Germany</option>
                     <option value="">Lodon United Kingdom</option>
-                    <option value="">Paris Italy</option>
+                    <option value="">Paris </option>
                   </select>
                 </div>
               </div>
